063f0846d8450879b8dfd27bc964fc5d5ea89b8d
fajar125 <fajar.rusius@gmail.com>
2016-04-06 11:46:44 +0700

    ''

--- server/lib/webi/users.php
+++ server/lib/webi/users.php
@@ -73,7 +73,59 @@ class wbUser extends Object{
 
         return $user_id;
     }
+	
+	public static function logInCard($cardNumber, $npwpd, $rememberMe = 0)
+    {
+        if (self::isLoggedIn()) return true;
 
+        if (empty($cardNumber)) throw new UserLoginFailedException('nomor kartu tidak boleh kosong');
+        if (empty($npwpd)) throw new UserLoginFailedException('npwpd tidak boleh kosong');
+
+        $dbconn = wbDB::getConn();
+        //$table = wbConfig::get('DB.prefix').'_user';
+        //$query = "SELECT user_id, user_name, user_password, user_email, user_realname, user_status FROM $table WHERE user_name = ?";
+        $query = "SELECT t_cust_account.t_cust_account_id,p_app_user_id 
+					from t_cust_acc_card
+						left join t_cust_account on t_cust_acc_card.t_cust_account_id = t_cust_account.t_cust_account_id
+						left join t_customer_user on t_cust_account.t_customer_id = t_customer_user.t_customer_id
+					where 
+						card_number = ? and t_cust_account.npwd = ?";
+		$result =& $dbconn->Execute($query,array($cardNumber, $npwpd));
+        if (!$result) return;
+        if ($result->EOF) {
+            $result->Close();
+            throw new UserLoginFailedException('User '.$userName.' belum terdaftar');
+        }
+		$table = 'sikp.p_app_user';
+        $query = "SELECT p_app_user_id as user_id,app_user_name as user_name,user_pwd as user_password,email_address as user_email,full_name as user_realname,p_user_status_id as user_status FROM $table WHERE p_app_user_id = ".$result->fields['p_app_user_id'];
+		$result =& $dbconn->Execute($query);
+		
+		
+		if($items_user['is_employee'] == 'Y'){
+			throw new UserLoginFailedException('User '.$userName.' belum terdaftar');
+		}
+        list($user_id, $user_name, $user_password, $user_email, $user_realname, $user_status) = $result->fields;
+        $result->Close();
+
+        if ($user_status != WB_STATUS_ACTIVE) throw new UserLoginFailedException('Account user anda tidak aktif. Silahkan hubungi administrator untuk info lebih lanjut');
+        $query = "select * from core_user_role where user_id = ?";
+
+        $result =& $dbconn->Execute($query,array($user_id));
+        $core_user_role = $result->fields;
+        if(empty($core_user_role['role_id'])){
+        
+            $query = "INSERT INTO public.core_user_role (user_id, role_id, main_role) VALUES (?, '2', '1')";
+    
+            $result =& $dbconn->Execute($query,array($user_id));
+        }
+        
+        $roles = self::getUserRoles($user_id);
+
+        self::setSession($user_id, $user_name, $user_email, $user_realname, $roles);
+
+        return $user_id;
+    }
+	
     public static function logOut()
     {
         if (!self::isLoggedIn()) return true;
